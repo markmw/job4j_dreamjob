6418c320c37b9e5e67b0e920ac42bbd2ef29cc08
markmw <dant1kkz@gmail.com>
2022-08-11 15:24:42 +0600

    4. Thymeleaf, Циклы. [#504841]

---
+++ src/main/java/ru/job4j/dream/store/PostStore.java
@@ -0,0 +1,39 @@
+package ru.job4j.dream.store;
+
+import ru.job4j.dream.model.Post;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class PostStore {
+    private static final PostStore INST = new PostStore();
+
+    private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
+
+    private PostStore() {
+        posts.put(1, new Post(
+                1,
+                "Junior Java Job",
+                "It's Java Junior work",
+                "2022-08-11"));
+        posts.put(2, new Post(
+                2,
+                "Middle Java Job",
+                "It's Java Middle work",
+                "2022-08-11"));
+        posts.put(3, new Post(
+                3,
+                "Senior Java Job",
+                "It's Java Senior work",
+                "2022-08-11"));
+    }
+
+    public static PostStore instOf() {
+        return INST;
+    }
+
+    public Collection<Post> findAll() {
+        return posts.values();
+    }
+}
